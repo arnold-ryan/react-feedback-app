31bd7d508d4223bf67e377d8589e9b168673dfd6
Arnold <arnold.ryan801@gmail.com>
2022-09-14 11:55:18 +0300

    added json-server mock backend

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,16 +2,19 @@ import FeedbackItem from "./FeedbackItem";
 // import PropTypes from "prop-types";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 // import { motion, AnimatePresence } from "framer-motion";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map((item) => (
         <FeedbackItem key={item.id} item={item} />
